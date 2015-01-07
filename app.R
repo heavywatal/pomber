34fb0df2bf37759a6cdabfcb9d00a2ea0d8b19b0
Watal M. Iwasaki <heavy.watal@gmail.com>
2015-01-07 17:46:39 +0900

    Single-file application

--- app.R
+++ app.R
@@ -6,7 +6,7 @@ library(ggplot2)
 
 tbl = read.delim('data/pombe-mean.tsv.gz', stringsAsFactor=FALSE)
 
-shinyServer(function(input, output) {
+server = function(input, output) {
   output$datatable = renderDataTable({
     tbl %>>% filter(nchar(oligo)==(input$width * 2 + 1),
         theta_pi<input$thr_p,
@@ -27,4 +27,19 @@ shinyServer(function(input, output) {
     labs(x=expression(theta[pi]), y='Tajima\'s D') +
     theme(legend.position='none')
   )
-})
+}
+
+ui = shinyUI(pageWithSidebar(
+  headerPanel("S. pombe polymorphism statistics"),
+  sidebarPanel(
+    numericInput('width', 'Width', 6, min=1, max=6),
+    numericInput('thr_p', HTML('Threshold for &theta;&pi;'), 0.007, min=0, max=1, 0.001),
+    numericInput('thr_d', 'Threshold for Tajima\'s D', -0.01, min=-2, max=2, 0.01)
+  ),
+  mainPanel(
+    plotOutput('plot'),
+    dataTableOutput("datatable")
+  )
+))
+
+shinyApp(ui, server)
